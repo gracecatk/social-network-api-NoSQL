9c6a5f9bd9558ff6b80ad236f898d85fe703b4e2
gracecatk <gracie.kiker@gmail.com>
2024-01-07 20:39:44 -0600

    finally got routes working

--- social-network-api-NoSQL/server.js
+++ social-network-api-NoSQL/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const db = require("./config/connection");
+const db = require("./config/connections");
 const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(routes);
 
 db.once("open", () => {
