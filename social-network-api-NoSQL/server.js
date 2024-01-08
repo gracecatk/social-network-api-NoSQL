aea17065b85e76ca7cfebd0c5d890202a019db23
gracecatk <gracie.kiker@gmail.com>
2024-01-07 20:15:05 -0600

    changed server.js

--- social-network-api-NoSQL/server.js
+++ social-network-api-NoSQL/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const db = require("./config/connections");
+const db = require("./config/connection");
 const routes = require("./routes");
 
-const PORT = 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 db.once("open", () => {
